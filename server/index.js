d252217a3eaa1c4ced58ba9039711fb1b35c5630
leo-cal <calasans.leonardo@gmail.com>
2024-07-17 12:53:49 -0300

    Changes to vercel config

--- server/index.js
+++ server/index.js
@@ -225,7 +225,7 @@ app.get('/searchwiki', async function(req, res) {
 
 });
 
-console.log('Listening on 8888');
-app.listen(8888);
+// console.log('Listening on 8888');
+// app.listen(8888);
 
 module.exports = app;
